0439e86118d26042dfeb01a3902486846b243329
IMANOL MUNOZ <rl200156@rlas-macair-29.rlas-116.org>
2018-12-12 11:38:45 -0600

    Initial Commit

---
+++ UiTextFieldChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UiTextFieldChallenge
+//
+//  Created by IMANOL MUNOZ on 10/4/18.
+//  Copyright © 2018 IMANOL MUNOZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var greetingAppLabel: UILabel!
+    @IBOutlet weak var sayHelloOnTapped: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    
+    
+    
+}
+
